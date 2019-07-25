3f7693fab7dad51f71ced9554811720ca87cfbbf
jasilven <jasilven@gmail.com>
2019-07-25 14:45:58 +0300

    file/path rename

--- scripts/test-load.sh
+++ scripts/test-load.sh
@@ -3,4 +3,8 @@
 # 2 threads
 # 10 HTTP connections open
 # keep 2000 req/s throughput
+echo "files list"
 wrk -t2 -c10 -d30s -R2000 -s test-load-config.lua https://127.0.0.1:8080/api/files
+
+echo "get file"
+wrk -t2 -c10 -d30s -R2000 -s test-load-config.lua https://127.0.0.1:8080/api/files/5548fb85-243d-4e2f-9650-b7dab6d30fd0
